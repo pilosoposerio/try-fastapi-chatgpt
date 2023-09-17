a20038bf8fea41685ca63b1c07e456a07803168c
Clinton Poserio <cposerio@appen.com>
2023-09-17 17:56:11 +0800

    Validate country using pycountry.

--- try_fastapi_chatgpt/application/api/fastapi.py
+++ try_fastapi_chatgpt/application/api/fastapi.py
@@ -1,21 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException.
+from typing_extensions import Annotated
 
 from try_fastapi_chatgpt.travel_recommendation.recommendation_strategy import (
     BoringTravelRecommendationStrategy,
 )
 from try_fastapi_chatgpt.travel_recommendation.travel_recommender import (
     TravelRecommender,
+    UnknownCountryError,
 )
 
 app = FastAPI()
 
-travel_recommender = TravelRecommender(
-    BoringTravelRecommendationStrategy()
-)
+travel_recommender = TravelRecommender(BoringTravelRecommendationStrategy())
 
 
 @app.get("/")
-async def root():
+def root():
     """
     API root path.
     """
@@ -23,20 +23,25 @@ async def root():
 
 
 @app.get("/recommend")
-async def recommend_travel_activities(country: str, season: str):
+def recommend_travel_activities(
+    country: Annotated[str, Query(min_length=2)],
+    season: Annotated[str, Query(min_length=3)],
+):
     """
     Recommends three travel activities for a given country and season
     :param country: Name of a country.
     :param season: Name of a season (or weather condition).
     :return: A JSON list of objects with the country, season, and one activity.
     """
-    return {
-        "country": country,
-        "season": season,
-        "recommendations": [
-            recommendation.recommendation
-            for recommendation in travel_recommender.recommend(
-                country, season
-            )
-        ],
-    }
+    try:
+        recommendations = travel_recommender.recommend(country, season)
+        return {
+            "country": recommendations[0].country,
+            "season": season,
+            "recommendations": [
+                recommendation.recommendation
+                for recommendation in recommendations
+            ],
+        }
+    except UnknownCountryError as exc:
+        raise HTTPException(detail=f"Unknown country: {country}")
--- try_fastapi_chatgpt/travel_recommendation/travel_recommender.py
+++ try_fastapi_chatgpt/travel_recommendation/travel_recommender.py
@@ -1,12 +1,17 @@
 from dataclasses import dataclass
 from typing import List
 
+import pycountry
 from try_fastapi_chatgpt.travel_recommendation.recommendation_strategy import (
     TravelRecommendationStrategy,
 )
 from try_fastapi_chatgpt.travel_recommendation.travel_recommendation import (
     TravelRecommendation,
 )
+
+
+class UnknownCountryError(ValueError):
+    pass
 
 
 @dataclass(frozen=True)
@@ -21,4 +26,10 @@ class TravelRecommender:
     def recommend(
         self, country: str, season: str
     ) -> List[TravelRecommendation]:
-        return self.recommender.recommend(country, season)
+        try:
+            country_info = pycountry.countries.search_fuzzy(country)[0]
+        except LookupError as exc:
+            raise UnknownCountryError(
+                f"No country matches: {country}"
+            ) from exc
+        return self.recommender.recommend(country_info.name, season)
